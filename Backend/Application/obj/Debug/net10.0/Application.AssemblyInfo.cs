1e1e99a662ab66effc5c145ea9fc07c65b9e8edb
Sergio R . Dominguez <51672861+serodominguez@users.noreply.github.com>
2026-07-07 11:20:52 -0400

    Update
    Update

--- Backend/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Backend/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff457d7fd0f97f85257c8f770d238502ae695f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b51426ef4c8219953b00b8365b590b133db497")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Backend/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff457d7fd0f97f85257c8f770d238502ae695f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b51426ef4c8219953b00b8365b590b133db497")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
